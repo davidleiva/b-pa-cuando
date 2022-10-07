3c1344dd054c9b1799033d79f8818fcb4ead5789
David Leiva <davidleivaherrero@gmail.com>
2022-10-07 17:24:08 +0200

    anal

--- gatsby-config.js
+++ gatsby-config.js
@@ -11,8 +11,6 @@ module.exports = {
         // You can add multiple tracking ids and a pageview event will be fired for all of them.
         trackingIds: [
           "G-EHCH1Q9576", // Google Analytics / GA
-          // "AW-CONVERSION_ID", // Google Ads / Adwords / AW
-          // "DC-FLOODIGHT_ID", // Marketing Platform advertising products (Display & Video 360, Search Ads 360, and Campaign Manager)
         ],
         // This object gets passed directly to the gtag config command
         // This config will be shared across all trackingIds
@@ -24,13 +22,13 @@ module.exports = {
         // This object is used for configuration specific to this plugin
         pluginConfig: {
           // Puts tracking script in the head instead of the body
-          head: true,
+          head: false,
           // Setting this parameter is also optional
-          respectDNT: false,
+          respectDNT: true,
           // Avoids sending pageview hits from custom paths
           exclude: ["/preview/**", "/do-not-track/me/too/"],
           // Defaults to https://www.googletagmanager.com
-          origin: "https://www.bellavistapacuando.es",
+          origin: "YOUR_SELF_HOSTED_ORIGIN",
         },
       },
     },
