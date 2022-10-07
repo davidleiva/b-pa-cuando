9f4abcdb01e21fb78e6f4d6146a26d4fc47e2662
David Leiva <davidleivaherrero@gmail.com>
2022-10-07 13:39:17 +0200

    css changes

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -13,59 +13,67 @@ const ExactNavLink = props => (
 
 const Navbar = ({ siteTitle }) => {
   return (
-    <nav class="navbar py-4 text-primary ">
+    <nav class="py-4 text-primary ">
       <div class="container">
-        <a class="navbar-brand logo-container">
-          <StaticImage
-            src="../images/bellavista-pa-cuando.svg"
-            width={172}
-            quality={100}
-            formats={["AUTO", "WEBP"]}
-            alt="A Gatsby astronaut"
-            className="img-fluid"
-          />
-        </a>
-        <div class="d-flex align-items-center bg-primary text-white p-2 rounded social-container">
-          <span>Síguenos en redes sociales</span>
-          <ul class="list-group list-group-horizontal list-group-flush ">
-            <li class="list-group-item bg-transparent" style={{border: 'none' }}>
-              <a target="_blank" href="https://www.instagram.com/bellavistapacuando/">
-                <StaticImage
-                  src="../images/instagram.png"
-                  width={48}
-                  quality={100}
-                  formats={["AUTO", "WEBP"]}
-                  alt="Instagram Bellavista pa'cuando"
-                  className="img-fluid"
-                />
-              </a>
-            </li>
-            <li class="list-group-item bg-transparent" style={{border: 'none' }}>
-              <a target="_blank" href="https://www.facebook.com/profile.php?id=100086093963788">
-                <StaticImage
-                  src="../images/facebook.png"
-                  width={48}
-                  quality={100}
-                  formats={["AUTO", "WEBP"]}
-                  alt="Facebook Bellavista pa'cuando"
-                  className="img-fluid"
-                />
-              </a>
-            </li>
-            <li class="list-group-item bg-transparent" style={{border: 'none' }}>
-              <a  target="_blank" href="https://twitter.com/Bellav_PaCuando">
-                <StaticImage
-                  src="../images/twitter.png"
-                  width={48}
-                  quality={100}
-                  formats={["AUTO", "WEBP"]}
-                  alt="Twitter Bellavista pa'cuando"
-                  className="img-fluid"
-                />
-              </a>
-            </li>
-          </ul>
+        <div className="row">
+          <div className="col-lg-7">
+            <a class="navbar-brand logo-container">
+              <StaticImage
+                src="../images/bellavista-pa-cuando.svg"
+                width={172}
+                quality={100}
+                formats={["AUTO", "WEBP"]}
+                alt="A Gatsby astronaut"
+                className="img-fluid"
+              />
+            </a>
+          </div>
+
+          <div class="col-lg-5">
+          <div className="d-flex align-items-center bg-primary text-white p-2 rounded social-container">
+            <span>Síguenos en redes sociales</span>
+            <ul class="list-group list-group-horizontal list-group-flush ">
+              <li class="list-group-item bg-transparent" style={{ border: 'none' }}>
+                <a target="_blank" href="https://www.instagram.com/bellavistapacuando/">
+                  <StaticImage
+                    src="../images/instagram.png"
+                    width={48}
+                    quality={100}
+                    formats={["AUTO", "WEBP"]}
+                    alt="Instagram Bellavista pa'cuando"
+                    className="img-fluid"
+                  />
+                </a>
+              </li>
+              <li class="list-group-item bg-transparent" style={{ border: 'none' }}>
+                <a target="_blank" href="https://www.facebook.com/profile.php?id=100086093963788">
+                  <StaticImage
+                    src="../images/facebook.png"
+                    width={48}
+                    quality={100}
+                    formats={["AUTO", "WEBP"]}
+                    alt="Facebook Bellavista pa'cuando"
+                    className="img-fluid"
+                  />
+                </a>
+              </li>
+              <li class="list-group-item bg-transparent" style={{ border: 'none' }}>
+                <a target="_blank" href="https://twitter.com/Bellav_PaCuando">
+                  <StaticImage
+                    src="../images/twitter.png"
+                    width={48}
+                    quality={100}
+                    formats={["AUTO", "WEBP"]}
+                    alt="Twitter Bellavista pa'cuando"
+                    className="img-fluid"
+                  />
+                </a>
+              </li>
+            </ul>
+          </div>
+          </div>
         </div>
+
       </div>
     </nav>
   )
--- src/pages/index.js
+++ src/pages/index.js
@@ -9,16 +9,16 @@ const IndexPage = () => (
   <Layout>
     <Seo title="Bellavista pa' cuando" />
     <section className="container text-primary">
-      <div className="row">
+      <div className="row align-items-end mb-5">
 
-        <div className="col-md-7">
+        <div className="col-lg-7">
 
           <h4 className="display-6 ftext" style={{ letterSpacing: '-0.5px' }}>Por una <span className="text-secondary">Bellavista digna</span></h4>
           <h2 className="display-4 mb-5 d-md-none">¡Únete al movimiento "#BellavistaPaCuando"!</h2>
           <h2 className="display-5 mb-5 d-none d-md-block d-xl-none" style={{ fontSize: '40px' }}>¡Únete al movimiento "#BellavistaPaCuando"!</h2>
           <h2 className="display-3 mb-5 d-none d-xl-block">¡Únete al movimiento "#BellavistaPaCuando"!</h2>
 
-          <div className="video-container mb-4 ratio ratio-16x9">
+          <div className="video-container mb-4 mb-lg-0 ratio ratio-16x9">
             <a href="https://youtu.be/RF87jAE0JmE" target="_blank" className="thbcontainer">
               <StaticImage
                 src="../images/portada.png"
@@ -41,33 +41,12 @@ const IndexPage = () => (
               allowfullscreen
             ></iframe> */}
           </div>
-
-          <ol className="custom">
-
-            <li className="mb-4">
-              <h5>Cuéntaselo a tus amigos y vecinos</h5>
-              <p>Necesitamos que se implique el mayor número de vecinos posible.Tú puedes ayudarnos, simplemente coméntaselo a amigos y conocidos.</p>
-            </li>
-
-            <li className="mb-4">
-              <h5>¡Síguenos en redes sociales y únete a nuestras acciones!</h5>
-              <p>Puedes seguirnos en <a href="https://www.instagram.com/bellavistapacuando/" target="_blank">instagram</a>, <a href="https://www.facebook.com/profile.php?id=100086093963788" target="_blank">facebook</a> y <a href="https://twitter.com/Bellav_PaCuando" target="_blank">twitter</a>. Allí podras seguir nuestras acciones y unirte a ellas para hacer más ruido y lograr una Bellavista digna.</p>
-            </li>
-
-            <li className="mb-4">
-              <h5>Usa el “hashtag” #BellavistaPaCuando” en redes sociales</h5>
-              <p>Comparte en redes sociales tus quejas sobre el estado del barrio. Si además, usas el hashtag #BellavistaPaCuando y etiquetas al ayuntamiento o a los responsables del gobierno, mejor.</p>
-            </li>
-
-
-
-          </ol>
         </div>
 
-        <div className="col-md-5">
+        <div className="col-lg-5">
           <div className="card bg-secondary text-white">
             <div className="card-body">
-              <h6 className="ftext text-primary">¡Sé uno más del movimiento!</h6>
+              <h4 className="ftext  text-primary">¡Sé uno más del movimiento!</h4>
               <h5 className="card-title h2 mb-4">¡Compra la camiseta y corre la voz!</h5>
               <div className="card-text mb-4">
                 <StaticImage
@@ -81,7 +60,7 @@ const IndexPage = () => (
               </div>
               <div className="d-flex">
                 <h6 className="h3 fsemi pe-2 w-50" style={{ fontSize: '2.5rem' }}>¡Sólo 9€!</h6>
-                <p className="w-50">Cobramos sólo <br />gastos de producción</p>
+                <p className="w-50 text-white">Cobramos sólo <br />gastos de producción</p>
               </div>
 
               {/* <!-- Button trigger modal --> */}
@@ -92,9 +71,29 @@ const IndexPage = () => (
 
           </div>
         </div>
-        <p className="text-center my-3">Bellavista Pa Cuando · Todos los derechos reservados</p>
       </div>
+      <div className="row">
+        <div className="col">
+          <ol className="custom">
+            <li className="mb-4">
+              <h5>Cuéntaselo a tus amigos y vecinos</h5>
+              <p>Necesitamos que se implique el mayor número de vecinos posible.</p>
+              <p>Tú puedes ayudarnos, simplemente coméntaselo a amigos y conocidos.</p>
+            </li>
 
+            <li className="mb-4">
+              <h5>¡Síguenos en redes sociales y únete a nuestras acciones!</h5>
+              <p>Puedes seguirnos en <a href="https://www.instagram.com/bellavistapacuando/" target="_blank">instagram</a>, <a href="https://www.facebook.com/profile.php?id=100086093963788" target="_blank">facebook</a> y <a href="https://twitter.com/Bellav_PaCuando" target="_blank">twitter</a>.</p><p>Allí podras seguir nuestras acciones y unirte a ellas para lograr una Bellavista digna.</p>
+            </li>
+
+            <li className="mb-4">
+              <h5>Usa el “hashtag” #BellavistaPaCuando” en redes sociales</h5>
+              <p>Comparte en redes sociales tus quejas sobre el estado del barrio.</p><p>Si usas el hashtag #BellavistaPaCuando y etiquetas al ayuntamiento o a los responsables del gobierno, mejor.</p>
+            </li>
+          </ol>
+        </div>
+      </div>
+      <p className="text-center my-3">Bellavista Pa Cuando · Todos los derechos reservados</p>
     </section>
     {/* <!-- Modal --> */}
     <div className="modal fade text-primary" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
